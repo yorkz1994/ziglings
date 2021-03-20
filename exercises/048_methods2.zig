53247f760094481641e9195fce7adbbd8ce632f2
yorkz1994 <york-z@outlook.com>
2021-03-20 20:19:09 +0800

    48

--- exercises/048_methods2.zig
+++ exercises/048_methods2.zig
@@ -24,7 +24,7 @@ const Elephant = struct {
 
     pub fn print(self: *Elephant) void {
         // Prints elephant letter and [v]isited
-        var v: u8 = if (self.visited) 'v' else ' ';
+        const v: u8 = if (self.visited) 'v' else ' ';
         std.debug.print("{u}{u} ", .{ self.letter, v });
     }
 };
@@ -54,7 +54,7 @@ fn visitElephants(first_elephant: *Elephant) void {
 
         // Get the next elephant or stop.
         if (e.hasTail()) {
-            e = e.???; // Which method do we want here?
+            e = e.getTail(); // Which method do we want here?
         } else {
             break;
         }
