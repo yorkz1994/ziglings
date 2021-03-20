f559c844e54892f2b87ec5ccecf6a7acc11c0322
yorkz1994 <york-z@outlook.com>
2021-03-20 21:30:59 +0800

    51

--- exercises/051_values.zig
+++ exercises/051_values.zig
@@ -92,7 +92,7 @@ pub fn main() void {
     // Let's assign the std.debug.print function to a const named
     // "print" so that we can use this new name later!
 
-    const print = ???;
+    const print = std.debug.print;
 
     // Now let's look at assigning and pointing to values in Zig.
     //
@@ -156,12 +156,12 @@ pub fn main() void {
 
     print("XP before:{}, ", .{glorp.experience});
 
-    levelUp(glorp, reward_xp);
+    levelUp(&glorp, reward_xp);
 
     print("after:{}.\n", .{glorp.experience});
 }
 
-fn levelUp(character_access: Character, xp: u32) void {
+fn levelUp(character_access: *Character, xp: u32) void {
     character_access.experience += xp;
 }
 
